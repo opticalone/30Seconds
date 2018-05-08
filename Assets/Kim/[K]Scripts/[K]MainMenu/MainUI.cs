88c0cbfdad05987ce665c53d9ab9fbd8c88dcbf0
Carelle <steamtik@gmail.com>
2018-05-08 07:21:28 -0700

    Quality and res works full screen needs work

--- Assets/Kim/[K]Scripts/[K]MainMenu/MainUI.cs
+++ Assets/Kim/[K]Scripts/[K]MainMenu/MainUI.cs
@@ -6,6 +6,28 @@ using UnityEngine.UI;
 
 public class MainUI : MonoBehaviour
 {
+    private void Start()
+    {
+        Resolution[] resolution;
+        resolution = Screen.resolutions;
+
+        res.onValueChanged.AddListener(delegate { Screen.SetResolution(resolution[res.value].width, resolution[res.value].height, Screen.fullScreen); });
+
+        for (int i = 0; i < resolution.Length; i++)
+        {
+            Dropdown.OptionData addedData = new Dropdown.OptionData(ResToString(resolution[i]));
+            res.options.Add(addedData);
+        }
+        qSlider.maxValue = 6;
+        qSlider.minValue = 1;
+        QualitySettings.SetQualityLevel(0);
+    }
+
+    private string ResToString(Resolution RTS)
+    {
+        return RTS.width + " x " + RTS.height;
+    }
+
     public void Load(string Scene)
     {
         SceneManager.LoadScene(Scene);
@@ -20,63 +42,44 @@ public class MainUI : MonoBehaviour
     public Slider qSlider;
     public Text qText;
 
-    private void Start()
-    {
-        Resolution[] resolution;
-        resolution = Screen.resolutions;
-
-        res.onValueChanged.AddListener(delegate { Screen.SetResolution(resolution[res.value].width, resolution[res.value].height, Screen.fullScreen); });
-
-        for (int i = 0; i < resolution.Length; i++)
-        {
-            Dropdown.OptionData addedData = new Dropdown.OptionData(ResToString(resolution[i]));
-            res.options.Add(addedData);
-        }
-
-        qSlider.maxValue = 6;
-        qSlider.minValue = 1;
-        QualitySettings.SetQualityLevel(0);
-
-
-    }
-
-    private string ResToString(Resolution RTS)
-    {
-        return RTS.width + " x " + RTS.height;
-    }
-
     public void graphics()
     {
-        Slider graphics = qSlider;
-        Text graphicsText = qText;
-        switch((int)graphics.value)
+        
+        switch((int)qSlider.value)
         {
             case 1:
                 QualitySettings.SetQualityLevel(0);
-                graphicsText.text = "Quality: Lowest";
+                qText.text = "Quality: Lowest";
+                Debug.Log(QualitySettings.GetQualityLevel());
                 break;
             case 2:
                 QualitySettings.SetQualityLevel(1);
-                graphicsText.text = "Quality: Low";
+                qText.text = "Quality: Low";
+                Debug.Log(QualitySettings.GetQualityLevel());
                 break;
             case 3:
                 QualitySettings.SetQualityLevel(2);
-                graphicsText.text = "Quality: Medium";
+                qText.text = "Quality: Medium";
+                Debug.Log(QualitySettings.GetQualityLevel());
                 break;
             case 4:
                 QualitySettings.SetQualityLevel(3);
-                graphicsText.text = "Quality: High";
+                qText.text = "Quality: High";
+                Debug.Log(QualitySettings.GetQualityLevel());
                 break;
             case 5:
                 QualitySettings.SetQualityLevel(4);
-                graphicsText.text = "Quality: Highest";
+                qText.text = "Quality: Highest";
+                Debug.Log(QualitySettings.GetQualityLevel());
                 break;
             case 6:
                 QualitySettings.SetQualityLevel(5);
-                graphicsText.text = "Quality: Ultra";
+                qText.text = "Quality: Ultra";
+                Debug.Log(QualitySettings.GetQualityLevel());
                 break;
         }
     }
+
     public void FullScreenToggle()
     {
         Screen.fullScreen = !Screen.fullScreen;
